7b9568382977cd7bb976642a8e5968837f2bf973
Vyacheslav <47851511+zerkel1991@users.noreply.github.com>
2022-08-25 21:57:56 +0400

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,99 @@
-import React from 'react';
+import React, { Children } from 'react';
 import { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
+import Btn from './button/Btn';
+import Modal from './Modal/Modal';
 import axios from 'axios';
-
+import { RotatingLines } from 'react-loader-spinner';
 
 class App extends Component {
   state = {
     imageName: '',
-    images :[],
-    page :1,
-    error :false,
-    loading:false,
+    images: [],
+    page: 1,
+    totalhits: 1,
+    loading: false,
+    showModal: false,
+    modalContent: ' ',
   };
-componentDidUpdate(_,prevState){
-
-  if(this.state.imageName !== prevState.imageName){
-    this.setState({loading: true})
-    axios.get(`https://pixabay.com/api/?q=${this.state.imageName}&page=${this.state.page}&key=28680775-d5d2c94020fa583a98c0796ee&image_type=photo&orientation=horizontal&per_page=12`)
-    .then(({data}) => {
-      this.setState({
-        images : data.hits
-      })
-    })
-    .catch(alert("Ничего не найдено :("))
-    .finally(this.setState({loading: false}))
-
+  componentDidUpdate(_, prevState) {
+    if (prevState.imageName !== this.state.imageName) {
+      this.setState({ loading: true });
+      axios
+        .get(
+          `https://pixabay.com/api/?q=${this.state.imageName}&page=${this.state.page}&key=29503826-8199b1172087c43264700da9d&image_type=photo&orientation=horizontal&per_page=12`
+        )
+        .then(({ data }) => {
+          this.setState(() => ({
+            images: [...data.hits],
+            page: 1,
+            totalhits: data.totalHits,
+          }));
+        })
+        .catch(error => console.log(error))
+        .finally(() => this.setState({ loading: false }));
+    }
+    if (prevState.page !== this.state.page) {
+      axios
+        .get(
+          `https://pixabay.com/api/?q=${this.state.imageName}&page=${this.state.page}&key=29503826-8199b1172087c43264700da9d&image_type=photo&orientation=horizontal&per_page=12`
+        )
+        .then(({ data }) => {
+          this.setState(({ images }) => ({
+            images: [...images, ...data.hits],
+            totalhits: data.totalHits,
+          }));
+        })
+        .catch(error => console.log(error))
+        .finally(() => this.setState({ loading: false }));
+    }
   }
-
-
-
-
-}
   handleSearchFormSubmit = imageName => {
     this.setState({ imageName });
+  };
+
+  loadmore = () => {
+    this.setState(({ page }) => ({
+      page: page + 1,
+    }));
+  };
+
+  openModal = modalContent => {
+    this.setState({
+      showModal: true,
+      modalContent,
+    });
+  };
+  closeModal = () => {
+    this.setState({ showModal: false });
   };
 
   render() {
     return (
       <>
         <Searchbar onSubmit={this.handleSearchFormSubmit} />
-        <ImageGallery images ={this.state.images}/>
+        {this.state.loading && (
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="96"
+            visible={true}
+          />
+        )}
+        {this.state.totalhits === 0 && <h1>Ничего не найдено</h1>}
+        <ImageGallery images={this.state.images} onClick={this.openModal} />
+        {this.state.images.length > 0 &&
+          this.state.images.length < this.state.totalhits && (
+            <Btn onClick={this.loadmore} />
+          )}
+
+        {this.state.showModal && (
+          <Modal closeModal={this.closeModal}>
+            <img src={this.state.modalContent.modalSrc} alt="reqPhoto" />
+          </Modal>
+        )}
       </>
     );
   }
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,10 +1,19 @@
 import s from './ImageGalleryItem.module.css'
-function ImageGalleryItem ({id,src}) {
+import PropTypes from 'prop-types';
+
+function ImageGalleryItem ({id,src,modalSrc,onClick}) {
   return(
-  <li className={s.ImageGalleryItem} key={id}>
+  <li onClick={()=>onClick({modalSrc})} className={s.ImageGalleryItem} key={id} >
   <img className={s.ImageGalleryItem_image} src={src} alt="photos" />
 </li>
   )
 }
+
+ImageGalleryItem.propTypes = {
+  id:PropTypes.number.isRequired,
+  src:PropTypes.string.isRequired,
+  modalSrc:PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default ImageGalleryItem
